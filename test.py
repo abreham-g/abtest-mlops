b6778af9cf832895f1045f675d04f2bcf037114a
abreham <aynuyeabresh@gmail.com>
2021-07-24 12:33:58 -0300

    Working on Test

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import dvc.api
 
 path = 'Data/ADSmartdata.csv'
@@ -9,3 +10,11 @@ data_url = dvc.api.get_url(
     repo=repo,
     rev=versio
     )
+data = pd.read_csv(data_url,sep=",")
+
+mlflow.log_params('data_url',data_url)
+mlflow.log_params('data_version',data_version)
+mlflow.log_params('data_rows',data.shape[0])
+mlflow.log_params('data_cols',data.shape[1])
+
+train,tast = train_tast_split(data)
